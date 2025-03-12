65830c73759625b2f781dc1e3c8678cd629bcf10
AnasAhmedFND <anasahmed4402@gmail.com>
2025-03-12 08:57:22 +0600

    hero navbar

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -62,7 +62,7 @@ const Hero = () => {
             <p className='md:mt-5 mt-3 '>I'm a professional <span className='font-bold'>Front-End Developer</span> and focused on crafting clean & user-friendly experiences. With expertise in Bootstrap, Tailwind CSS, React JS and Next JS. I bring <br /> innovative web solutions to life. Let's build something great together!</p>
 
             <div className="flex gap-3 md:w-[50%] items-center mt-6  relative ">
-              <button className='  border border-yellow-600  rounded-full pl-10 pr-14 py-4 text-[16px] hover:bg-yellow-600 after:duration-500 after:-right-[250px] after:ease-in-out '>MORE ABOUT ME</button>
+              <button className='  border border-yellow-600  rounded-full pl-10 pr-14 py-4 text-[16px] hover:bg-yellow-600 after:duration-500 after:right-[250px] after:ease-in-out hover:duration-700 '>MORE ABOUT ME</button>
               <p className=' w-[57px] h-[57px] bg-yellow-600 rounded-full flex justify-center items-center absolute left-[174px] text-[26px] '><FaArrowRight /></p>
             </div>
           </div>
@@ -71,15 +71,15 @@ const Hero = () => {
 
 
         <ul className={` md:w-[5%] flex flex-col gap-6 text-2xl md:static  px-2 ${menu ? 'absolute top-18 left-0 pb-14 bg-yellow-500 w-full duration-1000 ease-in-out ' : 'absolute top-19 -left-72 duration-700 ease-in-out '}`} >
-          <li className='mt-12 md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15]'> <FaHome className='w-[50px] ' /> <span className='font-bold md:hidden '>Home</span> </li>
+          <li className='mt-12 md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15] group'> <span className='md:absolute md:bg-yellow-600  md:rounded-full md:px-4 md:py-1 md:mr-[150px] hidden md:group-hover:block '>Home</span>  <FaHome className='w-[50px] ' /> <span className='font-bold md:hidden '>Home</span> </li>
 
-          <li className='md:flex-row-reverse  md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border md:border-[#ff55ff15] md:bg-[#ff55ff15] border-b pb-4 cursor-pointer '><IoIosContact className='w-[50px] text-3xl ' /><span className='font-bold md:hidden '>About</span> </li>
+          <li className='md:flex-row-reverse  md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border md:border-[#ff55ff15] md:bg-[#ff55ff15] border-b pb-4 cursor-pointer group'> <span className='md:absolute md:bg-yellow-600  md:rounded-full md:px-4 md:py-1 md:mr-[150px] hidden md:group-hover:block '>About</span> <IoIosContact className='w-[50px] text-3xl ' /><span className='font-bold md:hidden '>About</span> </li>
 
-          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15]'><MdOutlineBusinessCenter className='w-[50px] text-3xl ' /><span className='font-bold md:hidden'>Portfolio</span> </li>
+          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15] group'> <span className='md:absolute md:bg-yellow-600  md:rounded-full md:px-4 md:py-1 md:mr-[150px] hidden md:group-hover:block -right-[63px] '>Portfolio</span> <MdOutlineBusinessCenter className='w-[50px] text-3xl ' /><span className='font-bold md:hidden'>Portfolio</span> </li>
 
-          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15]'><FaDiagramProject className='w-[50px] ' /><span className='font-bold md:hidden'>Servics</span> </li>
+          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15] group'> <span className='md:absolute md:bg-yellow-600  md:rounded-full md:px-4 md:py-1 md:mr-[150px] hidden md:group-hover:block -right-[63px] '>Servics</span> <FaDiagramProject className='w-[50px] ' /><span className='font-bold md:hidden'>Servics</span> </li>
 
-          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15]'><HiOutlineMailOpen className='w-[50px] text-3xl ' /><span className='font-bold md:hidden'>Contact</span> </li>
+          <li className=' md:w-[50px] md:h-[50px] md:rounded-full md:flex md:justify-center md:items-center md:hover:bg-yellow-600 md:p-2 flex gap-4 items-center md:border-none border-b pb-4 cursor-pointer md:border md:border-[#f5f5] md:bg-[#ff55ff15] group'> <span className='md:absolute md:bg-yellow-600  md:rounded-full md:px-4 md:py-1 md:mr-[150px] hidden md:group-hover:block -right-[63px] '>Contact</span> <HiOutlineMailOpen className='w-[50px] text-3xl ' /><span className='font-bold md:hidden'>Contact</span> </li>
         </ul>
 
         <div className=" ">
