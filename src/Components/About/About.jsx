913962d0b750e31e8886b84e7c2443b7733990bd
AnasAhmedFND <anasahmed4402@gmail.com>
2025-03-12 19:52:19 +0600

    about respons

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { FaDownload } from "react-icons/fa";
+import Whon from './A-img/portfolio.jpg'
 
 const About = () => {
   return (
-    <section id='about' className='bg-[#212428] py-20 '>
+    <section id='about' className='bg-[#212428] md:py-20 py-10 px-2 md:px-0'>
 
       <div className="relative ">
-      <h2 className='text-6xl font-bold  text-white flex justify-center z-10'> ABOUT  <span className=' ml-3 text-yellow-600'> ME </span> </h2>
-      <h2 className='text-[110px] font-bold text-center text-white absolute -bottom-[50px] opacity-10  left-[410px] '>RECUME</h2>
+      <h2 className='md:text-6xl text-4xl font-bold  text-white flex justify-center z-10'> ABOUT  <span className=' ml-3 text-yellow-600'> ME </span> </h2>
+      <h2 className='md:text-[110px] text-[80px] font-bold text-center text-white absolute md:-bottom-[50px] -bottom-[40px] opacity-10  md:left-[410px] left-[10px] '>RECUME</h2>
       </div>
 
-      <div className=" text-white flex container mx-auto  gap-[5%] mt-28 ">
-        <div className="  w-[65%] py-10">
+      <img className='md:hidden block w-[200px] h-[210px] mx-auto mt-14 border-8 border-yellow-600 rounded-full ' src={Whon} alt="" />
+
+      <div className=" text-white md:flex container mx-auto  gap-[5%] md:mt-28 mt-10 ">
+        <div className="  md:w-[65%] md:py-10">
           <h2 className='text-3xl font-bold '>PERSONAL INFOS</h2>
 
-          <div className="flex gap-8 mt-8  bor">
-            <div className="text-xl flex flex-col gap-3 ">
+          <div className="flex justify-between md:gap-0 gap-5 mt-8  bor">
+            <div className="md:text-xl flex flex-col gap-3 md:w-1/2 ">
             <p>First Name :  <span className='font-bold'> Anas</span> </p>
             <p>Last Name :  <span className='font-bold'>Ahmed</span> </p>
             <p>Age :  <span className='font-bold'>21 Years</span> </p>
@@ -23,7 +26,7 @@ const About = () => {
             <p>Freelance :  <span className='font-bold'>Avaliable</span> </p>
 
             </div>
-            <div className="text-2xl flex flex-col gap-3 ">
+            <div className="md:text-xl flex flex-col gap-3 md:w-1/2 ">
               <p>Address : <span className='font-bold'>Dhaka </span> </p>
               <p>Phone : <span className='font-bold'>+8801580724723 </span> </p>
               <p>Email : <span className='font-bold'>anasahmed4402@gmail.com </span> </p>
@@ -32,21 +35,21 @@ const About = () => {
             </div>
 
           </div>
-          <div className="flex gap-5 items-center relative group  mt-10 w-[38%] *:">
-          <button className='border-2 border-yellow-600 font-bold   py-2 pl-7 pr-12 rounded-full group-hover:bg-yellow-600 hover:duration-300 '>DOWNLOAD RESUME  </button>
-          <p className=' w-[42px] h-[42px] rounded-full absolute flex justify-center items-center left-[206px]  bg-yellow-600'><FaDownload  className=' text-2xl  ' /></p>
+          <div className="flex gap-5 items-center relative group  mt-10 md:w-[38%] *:">
+          <button className='border-2 border-yellow-600 font-bold   py-2 pl-7 pr-12 rounded-full group-hover:bg-yellow-600 hover:duration-300 md:ml-0 ml-5 '>DOWNLOAD RESUME  </button>
+          <p className=' w-[42px] h-[42px] rounded-full absolute flex justify-center items-center md:left-[203px] left-[220px]  bg-yellow-600'><FaDownload  className=' text-2xl  ' /></p>
           </div>
 
         </div>
 
-        <div className="flex flex-col gap-5  w-[30%] ">
+        <div className="flex md:flex-col gap-5  md:w-[30%] md:mt-0 mt-8 ">
 
-          <div className="border-4 border-yellow-600 flex flex-col gap-5 w-full h-1/2 items-center rounded-2xl ">
+          <div className="border-4 border-yellow-600 flex flex-col gap-5 w-full md:h-1/2 items-center rounded-2xl ">
             <h3 className='text-6xl font-bold text-yellow-600 mt-5'>01+</h3>
-            <p>UEARS OF EXPERIENCE</p>
+            <p className='text-center'>UEARS OF EXPERIENCE</p>
           </div>
 
-          <div className="border-4 border-yellow-600 flex flex-col gap-5 w-full h-1/2 items-center rounded-2xl">
+          <div className="border-4 border-yellow-600 flex flex-col gap-5 w-full md:h-1/2 items-center rounded-2xl">
             <h3  className='text-6xl font-bold text-yellow-600 mt-5 '>15+</h3>
             <p>PROJECTS DONE</p>
           </div>          
