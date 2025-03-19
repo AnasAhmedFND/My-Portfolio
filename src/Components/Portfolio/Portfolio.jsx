7016f8337cfc05366f484a60d75c7ce81568aac8
AnasAhmedFND <anasahmed4402@gmail.com>
2025-03-19 19:26:17 +0600

    exclusive project link add

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -40,10 +40,12 @@ const Portfolio = () => {
           </div>
           </a>
 
+          <a href="https://exclusive-e-commerce-lsil.vercel.app/" target='_blanck' rel='noopener noopener' >
           <div className="relative md:mt-0 mt-5 ">
             <img className='md:w-[469px] md:h-[194px] h-[148px] rounded-2xl' src={Exclusive} alt="" />
             <h4 className='absolute -bottom-0 text-2xl font-bold -left-0 hover:bg-yellow-600 duration-700 ease-in-out border md:h-[194px] h-[148px] md:w-[469px] w-[344px] flex justify-center items-center  opacity-5 hover:opacity-90 rounded-2xl '>E-commerce Project</h4>
           </div>
+          </a>
 
 
         </div>
